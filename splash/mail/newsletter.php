3d67820f06c3297a93a18f3f591fdaa2cb6d313e
nconrad <nealconrad@gmail.com>
2015-03-23 12:01:03 -0500

    temp placeholder for newsletter

--- splash/mail/newsletter.php
+++ splash/mail/newsletter.php
@@ -2,13 +2,17 @@
 $recipient = "nconrad@anl.gov";
 $subject = "Subscription to coremodels.mcs.anl.gov";
 
-#$location = "enter the URL of the result page here";
+#$location = "URL";
 $sender = $recipient;
-$body = "This is an a newsletter subscription for coremodels.mcs.anl.gov\n";
+$body = "This is an a newsletter subscription for coremodels.mcs.anl.gov\n\n";
 $body .= "Email: ".$_REQUEST['email']." \n";
 
 #echo "$body";
-mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
+echo "<br>Thanks for subscribing!  <a href='/'>Go back</a>";
+
+mail( $recipient, $subject, $body, "From: $sender" ) or die ("<br><br>Mail could not be sent.");
+
+
 
 #header( "Location: $location" );
 ?>
